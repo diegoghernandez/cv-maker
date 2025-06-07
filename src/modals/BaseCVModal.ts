6bf5aae540e1603d875388a355f4080f5d0dad93
diegoghernandez <diegoJ4V@gmail.com>
2025-06-07 13:55:11 -0600

    Add modal to request the base resume

---
+++ src/modals/BaseCVModal.ts
@@ -0,0 +1,26 @@
+import { App, Modal, Setting } from "obsidian";
+
+export class BaseCVModal extends Modal {
+   constructor(app: App, onSubmit: (result: string) => void) {
+      super(app)
+      this.setTitle('Add your unformatted CV')
+
+      let cv = ''
+      new Setting(this.contentEl)
+         .setName('CV')
+         .setClass('cv__container')
+         .addTextArea((textArea) =>
+            textArea.onChange((value) => cv = value)
+         )
+
+      new Setting(this.contentEl)
+         .addButton((btn) =>
+            btn.setButtonText('Submit')
+               .setCta()
+               .onClick(() => {
+                  this.close()
+                  onSubmit(cv)
+               })
+         )
+   }
+}
